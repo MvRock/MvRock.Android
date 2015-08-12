fb62c23c07419930720624af133d0de642dbb863
TianhaoSong <lovetomoyosama@gmail.com>
2015-08-11 21:19:47 -0400

    GetMusicBuddy and GetRecBuddy

---
+++ project/MvRock_Sun_v11/mvRock_Sun_v11/src/main/java/com/mvrock/android/thread/GetMusicBuddyThread.java
@@ -0,0 +1,29 @@
+package com.mvrock.android.thread;
+
+import android.util.Log;
+
+import com.mvrock.android.model.MvRockModel;
+
+import org.apache.http.message.BasicNameValuePair;
+
+/**
+ * Created by Tianhao on 15/8/11.
+ */
+public class GetMusicBuddyThread extends MvRockThreadObject{
+
+    public GetMusicBuddyThread(String User_id, String Extra){
+        super(User_id, Extra);
+        this.TAG = "GetMusicBuddyThread";
+        this.Url = "/getMusicBuddy.php";
+    }
+    @Override
+    protected void setParams() {
+        Log.i(TAG, "setParams()");
+        params.add(new BasicNameValuePair("userid", User_id));
+    }
+
+    @Override
+    public void setResponse() {
+        MvRockModel.MusicBuddy.setHttpResponse(this.strResponse);
+    }
+}
